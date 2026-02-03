6b65ed12c04d0f4c9d3aecdc874f74e822e7863f
sheheryarali1-netizen <sheheryar.ali1@gomotive.com>
2026-02-03 20:12:05 +0500

    Explored interpolation attributes and event listeners

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,4 +11,12 @@ export class App {
   protected readonly data = signal({
     title: 'Angular Core Deep Dive',
   });
+
+  public onLogoClicked() {
+    alert('Hello World!');
+  }
+
+  public onKeyUp(value: string) {
+    this.data().title = value;
+  }
 }
