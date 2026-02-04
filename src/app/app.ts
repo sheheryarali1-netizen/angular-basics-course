b6214fdb480e1b73923d56d7c72044da5bd6d189
sheheryarali1-netizen <sheheryar.ali1@gomotive.com>
2026-02-04 09:37:10 +0500

    Created separate component for input field

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputField } from './input-field/input-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputField],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
