7dad73839b19eeb2b8c1f36141ea21f274874c14
sheheryarali1-netizen <sheheryar.ali1@gomotive.com>
2026-02-04 11:23:13 +0500

    Explored @for directive

--- src/app/resource-links/resource-links.ts
+++ src/app/resource-links/resource-links.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-resource-links',
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './resource-links.css',
 })
 export class ResourceLinks {
-
+  protected readonly links = signal([
+    { id: 1, title: 'Explore the Docs', link: 'https://angular.dev' },
+    { id: 2, title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    {
+      id: 3,
+      title: 'Prompt and best practices for AI',
+      link: 'https://angular.dev/ai/develop-with-ai',
+    },
+    { id: 4, title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    {
+      id: 5,
+      title: 'Angular Language Service',
+      link: 'https://angular.dev/tools/language-service',
+    },
+    { id: 6, title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ]);
 }
